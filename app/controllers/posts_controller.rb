bc245ffd844678d510a7cdf4fa4b5789a1f05974
dettyo <yu.rubysoho@gmail.com>
2024-03-22 22:52:00 +0900

    コメント追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,9 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content])
-    render json:{ post: post }
+    render json:{ post: post } #"json:"はjson option。
+    #変数postの値を、postというキーとセットでJavaScriptに送信
+    #JavaScriptでは受け取った変数postを使用して、メモをブラウザに反映
     #redirect_to action: :index
   end
 end
